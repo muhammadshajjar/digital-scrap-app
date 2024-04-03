4dc07b1e1106f899cef7bcf8c4cad96a3f236250
Muhammad Shajjar <muhammadshajjar99@gmail.com>
2024-04-03 14:02:21 +0500

    Implemented: ChatBot for customers and train it on sell scrap intent

--- app/customers/chat.jsx
+++ app/customers/chat.jsx
@@ -1,17 +1,139 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
-import { SafeAreaView } from "react-native-safe-area-context";
+import { COLORS } from "../../constants/Colors";
 
+// Import Dialogflow_V2 only if the platform is Android
+let Dialogflow_V2;
+if (Platform.OS === "android") {
+  Dialogflow_V2 = require("react-native-dialogflow").Dialogflow_V2;
+}
+
+import { dialogflowConfig } from "./env";
+
+const Bot = {
+  _id: 4,
+  name: "Digital Scrap Bot",
+  avatar: require("../../assets/images/chatbot-profile.png"),
+};
 
 const Chat = () => {
+  const [messages, setMessages] = useState([
+    {
+      _id: 1,
+      text: `Hello! Welcome to Digital Scrap. I'm here to assist you. How can I help you today?`,
+      createdAt: new Date(),
+      user: Bot,
+    },
+  ]);
 
+  useEffect(() => {
+    // Initialize Dialogflow only if the platform is Android
+    if (Platform.OS === "android") {
+      Dialogflow_V2.setConfiguration(
+        dialogflowConfig.client_email,
+        dialogflowConfig.private_key,
+        Dialogflow_V2.LANG_ENGLISH_US,
+        dialogflowConfig.project_id
+      );
+    }
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) => {
+      return GiftedChat.append(previousMessages, messages);
+    });
+
+    const sendBotResponse = (text) => {
+      let msg = {
+        _id: Math.random().toString(),
+        text,
+        createdAt: new Date(),
+        user: Bot,
+      };
+      setMessages((previousMessages) => {
+        return GiftedChat.append(previousMessages, [msg]);
+      });
+    };
+
+    const handleGoogleResponse = (result) => {
+      let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+      sendBotResponse(text);
+    };
+
+    // Only execute Dialogflow requestQuery if the platform is Android
+    if (Platform.OS === "android") {
+      let msg = messages[0].text;
+      Dialogflow_V2.requestQuery(
+        msg,
+        (result) => {
+          handleGoogleResponse(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      sendBotResponse(
+        "🌟 Thank you for reaching out to us! ✨ Unfortunately, at this moment, our AI-powered ChatBot 🤖 is only available on Android devices. We're actively working to bring this amazing feature to iOS as well! 📱 Stay tuned for exciting updates! In the meantime, feel free to explore other awesome features on our app! 💬🎉"
+      );
+    }
+  }, []);
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: COLORS.primaryGreen,
+          },
+        }}
+        textStyle={{
+          left: {
+            color: "#fff",
+            fontFamily: "Montserrat-Regular",
+          },
+          right: {
+            fontFamily: "Montserrat-Regular",
+          },
+        }}
+      />
+    );
+  };
   return (
-    <SafeAreaView>
-      <View>
-        <Text>chat</Text>
+    <View
+      style={{ flex: 1, paddingBottom: 100, backgroundColor: COLORS.primaryBg }}
+    >
+      <View
+        style={{
+          backgroundColor: COLORS.primaryGreen,
+          height: 90,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            marginTop: 50,
+            fontFamily: "Montserrat-SemiBold",
+            color: "white",
+            fontSize: 17,
+          }}
+        >
+          AI-Powered ChatBot 🤖
+        </Text>
       </View>
-    </SafeAreaView>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+        renderBubble={renderBubble}
+      />
+    </View>
   );
 };
 
---
+++ app/customers/env.js
@@ -0,0 +1,15 @@
+export const dialogflowConfig = {
+  type: "service_account",
+  project_id: "digital-scrap-bot",
+  private_key_id: "2d7bf9a6fa80f7a540fdb98ffebb8faa0a9cb079",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDevOVL5kB1NAuy\nbXs3YO4jlw/4or4KdR9mVzXMhTnvFLrf6FIxmg0A6yBmj0SaK5/7ec/VlyX1bSsx\nQSlDq2JMDvLogVWJHAPXpd2l29ftaC/Gbl4fYSDu/dE/Feov3rqJaaFqNfaQIj1a\nUKWzGGErzreTSjjEIUnn1wmW62R8AiAj4Cp2R4aIxOr0cYqei99Uf1XfyAk1QhBq\nEONYpJa7ZT9XJfGbXU/0JwA01gG0ShOG3knjgNZyhRnhm/+vIMz3Rfmi0rHFbDR5\nmzxvrXJ0SsAccjJuBgxVPgabHHaL6izbQl/hgTBeVBAjqVYNayKYcAe5RvYd/mJi\nL+QffYtJAgMBAAECggEAGHwa16pWvRuMyp8y245V90cYnBwluGgSHNGq6hHeIbw9\nY7Y57NZypf5y2BtZLvprMn18PY5JP66xwA8Z7mSlYFeHV8w53yHbgUXsNNqTJLee\ngtloktn+QQ9ZRG5QCVWaW1cnhZGrikIdTPOPF34vVl25u+D9lv/SzD/M/9BeAiyU\nIuZCJo4cearFW2ZPH9A4jV3mjaQdQBX6KxCpQA2e5Dp5MedMYYt9xyLypv1+o5ym\n4evaoJ5OZOX5lLna1TKFlS9ZkDjfA3Zn7RPzk0vcYX13bSUEj2D4bKnQiLx3a52g\nmUc93MjnUhdqLIZ2exg/yc2YRDlxfFyxiUIQ6xBDxQKBgQDygf0sXkEzytatQTpP\nrZxLMbGd3tzgp6i7RqR8KJO896HzY7/foGo7yEakqpu5aF1dlP+POYCS1bTmLPFM\nNGM2S72eDF1h60kZhWXjiMjgx1ozgI7Cc84I4WN+63qRIiAu9cPncCximf+jeXTk\nwl16U1JCed0GMNXv0PmWRCpBzQKBgQDrIVOJpQ+fkRF/1S5PaeIf7QYPylBSAYgE\nqPEayihAi4Hu/OC3c96OE72Ki6cX9PYQve/NQi4jiQr+ovbHTk9MCkLnzfu9YCZX\nx+RpbPMGe6ZUUXaWdMbH2PDmofXgZmKIu8FlFZSVqHHNWQMqLXQpNJH5mLj3sFEB\nRpzOZgKjbQKBgD4bLEuAmBI7kF8upVBilfRr9aEIwmiGh5/up5FkkvGCOeerVChh\nXNOSpwTk7Jy/LaoZzplPkK4KLRoFyIZLpFzzaMxhPkkliMMQdtee4FMwutkVw3np\nTorXrjOC+xDQPI3KQzGyeKuH5QBoypcLcAUz8Y00yHYda6zPSBxDuM7BAoGBALVk\nlWzH4SCpqsVisgJj3iXhFl8SJhrNwaBOsQqI0LrSpV+4yKfehOA4zG3YxQSPX1GV\nJi3Ek5GohMni8NzbRgOUmv73gvEa/xmTRVwrx+TFeZDs876owj2k2FwksGn4sqVn\nq5APMPyv4XOY6v8b59RuEPtsXa3/4M5KKmh0fC6VAoGAZuRxKil7ZBn4zBKX1lW0\nlOadHULMOy6seDqCxc2QB0CR+tawb+zdd0QKW58SGVyW2EWRz5gArVWCXIEDj3Ir\nXt+Mkw1coopQZCjmO1ViWR2AMHpthDWDw/0QvszCrdj+pvh+tXTd7FePxsqUMEYU\nLbSLLIFWdmVmQT9+ivlsONs=\n-----END PRIVATE KEY-----\n",
+  client_email: "digitalscrapbot@digital-scrap-bot.iam.gserviceaccount.com",
+  client_id: "109000151727941249796",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/digitalscrapbot%40digital-scrap-bot.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com",
+};
