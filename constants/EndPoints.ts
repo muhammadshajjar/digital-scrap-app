b80b27d13c45a9a983aae6fb9c01ac67ccb4ac2b
Muhammad Shajjar <muhammadshajjar99@gmail.com>
2024-05-14 14:36:08 +0500

    feat: deploy firebase cloud function for strip client side secret

--- constants/EndPoints.ts
+++ constants/EndPoints.ts
@@ -1,5 +1,5 @@
-
-
 //USE LOCAL SERVER WHILE DEVELOPING AS FIREBASE CHARGE WHEN DEPLOY THE FUNCTION
 
-export const STRIPE_FIREBASE_SERVER="http://127.0.0.1:5001/digital-scrap-dc471/us-central1/completePaymentWithStripe"
+// export const STRIPE_FIREBASE_SERVER="http://127.0.0.1:5001/digital-scrap-dc471/us-central1/completePaymentWithStripe"
+export const STRIPE_FIREBASE_SERVER =
+  "https://completepaymentwithstripe-bubmhbxyca-uc.a.run.app";
